1fc3917c0b15daedba8c7d2e7bdaf8172c356fbf
asimsedhain <asimsedhain33@gmail.com>
2020-06-02 03:48:31 -0500

    added GET and POST method

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const objectId = require("mongoose").objectId
+const Portfolio = require("../models/portfolio")
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+	res.send(JSON.stringify({ "error": "Please insert ID" }))
 });
+
+router.get("/:id", async (req, res) => {
+	try {
+		const user = await Portfolio.findById(objectId(req.param.id))
+		if (user) {
+			res.json(user)
+		} else {
+			throw "User is null"
+		}
+	} catch (error) {
+		console.log(`${new Date().toLocaleString()}: ${error}`)
+		res.statusCode = 401
+		res.json({ "error": "Invalid ID" })
+	}
+})
+
+router.post("/:id", async (req, res) => {
+	try {
+		const user = new Portfolio(req.body)
+		const info = await user.save()
+		res.json({ user, info })
+	} catch (error) {
+
+		console.log(`${new Date().toLocaleString()}: ${error}`)
+		res.statusCode = 401
+		res.json({ "error": "Invalid ID" })
+
+	}
+})
 
 module.exports = router;
