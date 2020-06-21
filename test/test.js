f4f3995dd88f9bc1ada500ab56485ba67ce36dc0
Ashim Sedhain <asimsedhain33@gmail.com>
2020-06-21 04:23:10 -0500

    fix: fixed tests to the current api

--- test/test.js
+++ test/test.js
@@ -7,8 +7,52 @@ mongoose.Promise = global.Promise;
 const DB_URL = "mongodb://localhost:27017"
 
 
-const vaild_doc = [{ Name: "hey", "Bio": "Hey I am Test", Projects: [], Education: [], SocialMediaLinks: [], Experiences: [] }, { Name: "Hey", Bio: "I am another test" }]
-const invalid_doc = [{ "Bio": "Hey I am Test", Projects: [], Education: [], SocialMediaLinks: [], Experiences: [] }, { Name: "Test", Projects: [], Education: [], SocialMediaLinks: [], Experiences: [] }]
+const vaild_doc = [{ fullName: "hey", "bio": "Hey I am Test", projects: [], education: [], experiences: [] }, { fullName: "Hey", bio: "I am another test" }, {
+	fullName: 'Susan Lee',
+	email: 'SLee@gmail.com',
+	bio: 'Web Developer at FANNG',
+
+	github: 'github.com/SLee',
+	linkedin: '',
+
+	skills: ["Git", "JavaScript", "Docker", "Python", "HTML", "Databases", "Go"],
+
+	projects: [{
+		Title: "Halting Machine",
+		Description: "A machine that answers if an algorithm will halt or not. Created to answer the age old question of reality.",
+		SourceLink: "github.com/slee/haltingmachine",
+		DemoLink: "slee.github.io",
+		Highlights: ["Solves P vs NP problem.", "Won the Turning Award.", "Used Java, Python, C, Assembly, and every language known to man."],
+		StartDate: "May 2020",
+		EndDate: "June 2020"
+	}],
+	experiences: [{
+		CompanyName: "Amazon",
+		Position: "Web Developer",
+		StartDate: "June 2018",
+		EndDate: "Current",
+		Highlights: ["Created the front website.", "Redesigned the color scheme."]
+	}, {
+		CompanyName: "Google",
+		Position: "ML Engineer",
+		StartDate: "June 2020",
+		EndDate: "Current",
+		Highlights: ["Lead the Google Brain Team", "Made Billion dollar in profit."]
+	}],
+	education: [{
+		InstituteName: "Princton University",
+		StartDate: "May 2015",
+		EndDate: "Aug 2019",
+		Degree: "Phd in Computer Science",
+		Courses: ["Big Data", "Machine Learning", "Operating Systems", "Distributed Systems"]
+	}, {
+		InstituteName: "Harvard University",
+		StartDate: "May 2010",
+		EndDate: "Aug 2014",
+		Degree: "Bsc in Computer Science",
+		Courses: ["A.I.", "Linear Algebra"]
+	}],
+}]
 
 
 
@@ -38,7 +82,7 @@ describe("API Tests", () => {
 				expect(res.body).to.have.property("error")
 			})
 
-			
+
 		})
 
 
@@ -61,15 +105,6 @@ describe("API Tests", () => {
 				})
 			})
 
-
-			// Testing with incorrect data
-			invalid_doc.map((doc) => {
-				it("should not post data and should return an error", async () => {
-					const res = await request(app).post("/users").send(doc)
-					expect(res.statusCode).to.equal(401)
-					expect(res.body).to.have.property("error")
-				})
-			})
 
 
 		})
