f0c0a3c4e916717d138c599e9aead33e6ce11578
asimsedhain <asimsedhain33@gmail.com>
2020-07-08 04:21:07 -0500

    fix(webtokens): fix verifyWebTokens middlewear to accept 'Bearer TOKEN' format

--- middleware/webTokensMiddleware.js
+++ middleware/webTokensMiddleware.js
@@ -2,10 +2,15 @@ const createError = require("http-errors")
 const { verifyRefreshToken, verifyAccessToken } = require("../utility/webTokens")
 
 const verifyAccessTokenMiddleware = (req, res, next) => {
-	const encodedToken = req.headers["authorization"]
+	const header = req.headers["authorization"]
+	if(!header){
+		next(createError(401))
+		return
+	}
 	try {
-
-		const token = verifyAccessToken(encodedToken)
+		// header comes in "Bearer TOKEN" format.
+		// we split the token and decode it
+		const token = verifyAccessToken(header.split(' ')[1])
 		req.userName = token.name
 		req.userId = token.id
 		next()
