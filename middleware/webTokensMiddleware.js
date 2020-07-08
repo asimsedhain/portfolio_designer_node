09a1a2834f5f35fde6ac5785eb594d830c171993
asimsedhain <asimsedhain33@gmail.com>
2020-07-08 04:25:50 -0500

    refactor(verify web tokens middleware): streamline logic and run auto formatter

--- middleware/webTokensMiddleware.js
+++ middleware/webTokensMiddleware.js
@@ -1,42 +1,42 @@
-const createError = require("http-errors")
-const { verifyRefreshToken, verifyAccessToken } = require("../utility/webTokens")
+const createError = require("http-errors");
+const {
+	verifyRefreshToken,
+	verifyAccessToken,
+} = require("../utility/webTokens");
 
 const verifyAccessTokenMiddleware = (req, res, next) => {
-	const header = req.headers["authorization"]
-	if(!header){
-		next(createError(401))
-		return
+	const header = req.headers["authorization"];
+	if (!header) {
+		next(createError(401));
+		return;
 	}
 	try {
 		// header comes in "Bearer TOKEN" format.
 		// we split the token and decode it
-		const token = verifyAccessToken(header.split(' ')[1])
-		req.userName = token.name
-		req.userId = token.id
-		next()
+		const token = verifyAccessToken(header.split(" ")[1]);
+		req.userName = token.name;
+		req.userId = token.id;
+		next();
 	} catch (error) {
-		console.log(error)
-		next(error)
+		console.log(error);
+		next(error);
 	}
-}
-
+};
 
 const verifyRefreshTokenMiddleware = (req, res, next) => {
-	const encodedToken = req.cookies.rwt
-	if (encodedToken) {
-		try {
-			const token = verifyRefreshToken(encodedToken)
-			req.userId = token.id
-			next()
-		} catch (error) {
-			
-			console.log(error)
-			next(createError(401))
-		}
-	}else{
-		next(createError(401))
+	const encodedToken = req.cookies.rwt;
+	if (!encodedToken) {
+		next(createError(401));
+		return
 	}
-}
+	try {
+		const token = verifyRefreshToken(encodedToken);
+		req.userId = token.id;
+		next();
+	} catch (error) {
+		console.log(error);
+		next(createError(401));
+	}
+};
 
-
-module.exports = { verifyAccessTokenMiddleware, verifyRefreshTokenMiddleware }
+module.exports = { verifyAccessTokenMiddleware, verifyRefreshTokenMiddleware };
