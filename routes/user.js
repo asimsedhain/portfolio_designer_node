06c5f3ba07d0922091f693db18250fa647195cba
asimsedhain <asimsedhain33@gmail.com>
2020-07-09 22:30:00 -0500

    feat(user route): add portfolio routes

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const createError = require("http-errors")
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user')
+const Portfolio = require("../models/portfolio")
+const objectId = require("mongoose").Types.ObjectId
 const { decode } = require('jsonwebtoken')
 const { createAccessToken, createRefreshToken, verifyAccessToken } = require("../utility/webTokens");
 const { verifyRefreshTokenMiddleware, verifyAccessTokenMiddleware} = require("../middleware/webTokensMiddleware")
@@ -21,16 +23,47 @@ router.get("/logout", async (req, res)=>{
 	res.json({status: "ok"})
 })
 
-//GETs all the portfolios for the user
-router.get("/portfolios", verifyAccessTokenMiddleware, async (req, res) =>{
-	
+// GETs all the portfolios for the user
+router.get("/portfolios", verifyAccessTokenMiddleware, async (req, res, next) =>{
+	try{
+		const portfolios = await Portfolio.find({userId: req.userId}, "_id portfolioName")
+		res.json(portfolios)
+	}catch(error){
+		console.log(`${new Date().toLocaleString()}: ${error}`)
+		next(createError(400))
+	}	
 })
 
-//GETs the portfolio with the given id in query
-router.get("/portfolio", verifyAccessTokenMiddleware, async (req, res)=>{
-	res.json({ok: "ok"})
-
+// GETs the portfolio with the given id in query
+router.get("/portfolio", verifyAccessTokenMiddleware, async (req, res, next)=>{
+	try{
+		const query = {_id: objectId(req.query.id), userId: req.userId}
+		const portfolio = await Portfolio.findOne(query, "-userId").exec()
+		console.log(portfolio)	
+		if(portfolio){
+			res.json(portfolio)
+		} else{
+			throw createError(400)
+		}
+	}catch (error){
+		console.log(`${new Date().toLocaleString()}: ${error}`)
+		next(createError(400))
+	}
 })
+
+// POSTs new portfolio for the user 
+router.post("/portfolio", verifyAccessTokenMiddleware, async (req, res, next)=>{
+	try{
+		const portfolioObject = {...req.body, userId: req.userId}
+		const portfolio = new Portfolio(portfolioObject)
+		await portfolio.save()
+		res.json({status: "success"})
+	} catch (error){
+		console.log(`${new Date().toLocaleString()}: ${error}`)
+		next(createError(400))
+	}
+})
+
 
 
 module.exports = router
